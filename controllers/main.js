2f60395014ba58c44b016ebab61d2f9ee5677109
Josh Terrell <josh@ifixit.com>
2014-04-15 19:04:13 +0000

    Allow return to page via browser back-button
    In order to allow returning via the browser's back button, we can either reload
    the page completely as if it was a normal load, or do stuff client side to
    effectively do the same. This was (to me) the more straight-forward solution.

--- controllers/main.js
+++ controllers/main.js
@@ -2,6 +2,16 @@ var socketAuthenticator = require('../lib/socket-auth');
 
 module.exports = {
    index: function(req, res) {
+
+      // Allow returning to the page via backspace to work
+      // When the user returns to the page, he usually gets a browser-cached
+      //  version. This tells the browser not to cache the page so that it is
+      //  always reloaded
+      // http://stackoverflow.com/a/6098245
+      res.header('Cache-Control',
+       'no-cache, private, no-store, must-revalidate, max-stale=0,' +
+       'post-check=0, pre-check=0');
+
       res.render('home/index', {
          socketToken: socketAuthenticator.getTokenForUser(req.user)
       });
